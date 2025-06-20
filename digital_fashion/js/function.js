056eec8d41c46198b5b57f2eb931ee98b63e073b
JabissyD <diana07jabalaka@gmail.com>
2025-06-20 15:07:06 +0300

    Add files via upload

---
+++ digital_fashion/js/function.js
@@ -0,0 +1,47 @@
+'use strict';
+
+/**
+* Фильтр приложения
+*
+* @return {undefined}
+*/
+function app() {
+  const buttons = document.querySelectorAll('.products__link');
+  const cards = document.querySelectorAll('products__item');
+
+  /**
+  * Фильтр элементов
+  *
+  * @param {string} category категория
+  * @param {object} items карточки товаров
+  *
+  * @return {undefined}
+  */
+  function filter(category, items) {
+    items.forEach((item) => {
+      const isItemFiltered = !item.classList.contains(category);
+      const isShowAll = category.toLowerCase() === 'all';
+      if (isItemFiltered && !isShowAll) {
+        item.classList.add('anime');
+      } else {
+        item.classList.remove('hide');
+        item.classList.remove('anime');
+      }
+    });
+  }
+
+  buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+      const currentCategory = button.dataset.filter;
+      filter(currentCategory, cards);
+    });
+  });
+
+  cards.forEach((card) => {
+    card.ontransitionend = function () {
+      if (card.classList.contains('anime')) {
+        card.classList.add('hide');
+      }
+    };
+  });
+}
